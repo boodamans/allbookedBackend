8070745fefa3ea9483415586213ce42ff2de70fa
Sam Narciso <samnarciso@Sams-MacBook-Pro.local>
2023-12-20 12:40:44 -0500

    added app test file

---
+++ app.test.js
@@ -0,0 +1,14 @@
+const supertest = require('supertest');
+const app = require('./app');
+
+describe('Express App Tests', () => {
+  it('should handle 404 errors', async () => {
+    const response = await supertest(app).get('/nonexistent-route');
+
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({
+      error: { message: 'Not Found', status: 404 },
+    });
+  });
+
+});
